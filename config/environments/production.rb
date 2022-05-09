9ff4b3ec02a22ed308f5b8af6af225de28ccf75c
GabrielValle91 <gvalle91@gmail.com>
2022-05-09 14:24:48 -0500

    prod mailer update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #need to update to hosting url before app goes live
-  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://radiant-thicket-85951.herokuapp.com' }
 end

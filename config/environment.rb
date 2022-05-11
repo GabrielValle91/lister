cd48147ebe763da6b03cb7eef2640532e783ddaa
GabrielValle91 <gvalle91@gmail.com>
2022-05-11 15:28:11 -0500

    add ddefault from email

--- config/environment.rb
+++ config/environment.rb
@@ -11,5 +11,6 @@ ActionMailer::Base.smtp_settings = {
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+    :enable_starttls_auto => true,
+    :default_from_email => ENV['FROM_EMAIL_ADDRESS']
 }
